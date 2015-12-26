38bf6f4bdf462f1e9eb300ff21c4373439aaf561
dmicic <darko@micic.ch>
2015-12-26 18:22:10 +0100

    Very init commit

---
+++ BisStat/BisStat.Data/BisProvider.fs
@@ -0,0 +1,84 @@
+﻿namespace Bis.Data.Provider
+
+open System.IO
+open System.Linq
+open System.Reflection
+
+open Microsoft.FSharp.Core.CompilerServices
+open ProviderImplementation.ProvidedTypes
+
+open Microsoft.FSharp.Quotations
+
+open System.Collections.Generic
+
+open BisStat.Data.Parser
+
+[<TypeProvider>]
+type public CbsProvider(cfg:TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces()
+
+    let ns = "Bis"
+    let asm = Assembly.GetExecutingAssembly()
+
+    let datasetProvider = 
+        let ds = ProvidedTypeDefinition(asm, ns, "Dataset", Some typeof<obj>)
+            
+        ds.AddMember <| ProvidedConstructor(parameters = [], InvokeCode = fun args -> <@@ new Dictionary<string, string list>() @@>)
+        ds.AddMember <| ProvidedMethod("Filter", [ProvidedParameter("obsFilter", typeof<Dictionary<string,string list>>)], typeof<string[] list>, 
+                        InvokeCode = (fun [me;obsFilter] -> <@@
+                                                                let parser = new CbsParser(@"C:/Users/Darko/Desktop/full_BIS_CBS_csv.csv")
+                                                                parser.filter ((%%obsFilter : Dictionary<string, string list>))
+                                                            @@>))
+        ds
+        
+    let mutable pathToDatasetFile = @"C:/Users/Darko/Desktop/full_BIS_CBS_csv.csv"
+                                          
+    let parameters = [ProvidedStaticParameter("PathToDatasetFile", typeof<string>)]
+
+    let enumProvider =  
+        let set = CbsParser(pathToDatasetFile).getDataset
+
+        let types = set.dimensions
+                        |> Seq.map (fun d -> let p = ProvidedTypeDefinition(asm, ns, d.name, Some typeof<obj>)
+                                             d.members
+                                                |> Seq.map (fun m -> ProvidedLiteralField(m, typeof<string>, m.Substring(0, m.IndexOf(':'))))
+                                                |> Seq.iter (fun m -> p.AddMember(m))
+                                             p)
+                        |> Seq.toList
+        types
+
+    let filterProvider =
+        let set = CbsParser(pathToDatasetFile).getDataset
+
+        let fp = ProvidedTypeDefinition(asm, ns, "Filter", Some typeof<obj>, HideObjectMethods = true)
+        fp.AddMember <| ProvidedConstructor(parameters = [], InvokeCode = fun args -> <@@ new Dictionary<string, string list>() @@>)
+
+        set.dimensions.Select(fun x -> x.name)
+            |> Seq.map (fun d -> ProvidedProperty (d, 
+                                                    typeof<string list>, 
+                                                    IsStatic = false, 
+                                                    GetterCode = (fun args -> <@@ let dict = ((%%args.[0] : obj) :?> System.Collections.Generic.Dictionary<string,string list>)
+                                                                                  if not (dict.ContainsKey d) then dict.Add (d, [])
+                                                                                  dict.[d]
+                                                                               @@>),
+                                                    SetterCode = (fun args -> <@@ ((%%args.[0] : obj) :?> System.Collections.Generic.Dictionary<string,string list>).[d] <- (%%args.[1] : string list) @@>)))
+            |> Seq.toList
+            |> fp.AddMembers
+
+
+        let mt = ProvidedMethod("Get", [], typeof<Dictionary<string,string list>>)
+        mt.InvokeCode <- (fun args -> <@@ let dict = ((%%args.[0] : obj) :?> System.Collections.Generic.Dictionary<string,string list>)
+                                          let ret = new Dictionary<string, string list>()
+                                          for f in dict.Where((fun d -> d.Value.Length > 0)) do
+                                              ret.Add(f.Key, f.Value)
+                                          ret
+                                        @@>)
+        fp.AddMember (mt)
+
+            
+        fp
+
+    do this.AddNamespace(ns, enumProvider.Union([datasetProvider;filterProvider]) |> Seq.toList)    
+
+[<assembly:TypeProviderAssembly>]
+do ()
---
+++ BisStat/BisStat.Data/DatasetPrasers.fs
@@ -0,0 +1,90 @@
+﻿namespace BisStat.Data.Parser
+
+open System.Collections.Generic
+open System.IO
+open System.Linq
+
+open FSharp.Data
+
+type public Dimension(dimensionName: string, memberList: string[]) =  
+    class
+        member this.name = dimensionName
+        member this.members = memberList
+    end
+
+type Dataset(dimensions: Dimension[]) =
+    class 
+        member this.dimensions = dimensions
+    end
+        
+type dimPos = { dim : string; pos : int; members : string list }
+        
+[<AbstractClass>]
+type public Parser(filePath: string) = 
+    let data = CsvFile.Load(filePath, ",", '"',true,false, 8).Cache()
+        
+    abstract member headerRowCount : int
+
+    member x.getDataset = 
+            let lines = File.ReadLines(filePath)
+
+            let headers = lines
+                            |> Seq.skip x.headerRowCount
+                            |> Seq.take 1 
+                            |> Seq.collect (fun h -> h.Split ([|"\",\""|], System.StringSplitOptions.RemoveEmptyEntries))
+                            |> Seq.takeWhile (fun d -> d <> "Time Period")
+                            |> Array.ofSeq
+
+            let observations = lines
+                                |> Seq.skip (x.headerRowCount + 1)
+                                |> Seq.map (fun o -> o.Split([|"\",\""|], headers.Length + 1, System.StringSplitOptions.RemoveEmptyEntries))
+                                |> Array.ofSeq
+                
+            let dimensions = [1 .. headers.Length - 1]
+                                |> Seq.mapi (fun i d -> observations
+                                                            |> Seq.map (fun o -> Array.get o i)
+                                                            |> Seq.distinct
+                                                            |> Array.ofSeq)
+                                |> Seq.mapi (fun i d -> new Dimension ((Array.get headers i), d))
+                                |> Array.ofSeq
+                
+            new Dataset(dimensions)
+
+
+    member x.filter (obsFilter:Dictionary<string, string list>) =
+            let lines = File.ReadLines(filePath)
+
+            let headers = lines
+                            |> Seq.skip x.headerRowCount
+                            |> Seq.take 1 
+                            |> Seq.collect (fun h -> h.Split ([|"\",\""|], System.StringSplitOptions.RemoveEmptyEntries))
+                            |> Seq.takeWhile (fun d -> d <> "Time Period")
+                            |> Seq.toArray
+
+            let dims =  headers
+                        |> Seq.mapi (fun i d -> { dim = d; pos = i; members = if obsFilter.ContainsKey(d) then obsFilter.[d] else [] })
+                        |> Seq.filter (fun f -> f.members.Length > 0)
+                        |> Seq.toArray
+
+            let headerCount = headers.Count()
+
+            let filtered = lines
+                                |> Seq.skip (x.headerRowCount + 1)
+                                |> Seq.map (fun o -> o.Split([|"\",\""|], System.StringSplitOptions.RemoveEmptyEntries))
+                                |> Seq.filter (fun o -> let obsArr = o.[headerCount].Split ([|':'|], System.StringSplitOptions.RemoveEmptyEntries)
+                                                        dims
+                                                            |> Seq.filter (fun flt -> flt.members.Contains(obsArr.[flt.pos]))
+                                                            |> Seq.length = dims.Count()
+                                                )
+                                |> Seq.map (fun o -> o.Skip(headerCount).ToArray())
+                                |> List.ofSeq
+
+            filtered
+
+type public CbsParser(filePath) =
+    inherit Parser(filePath)
+    override this.headerRowCount = 8
+
+type public LbsParser(filePath) =
+    inherit Parser(filePath)
+    override this.headerRowCount = 7
